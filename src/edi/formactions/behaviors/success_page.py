0a9a01953006b4d441deb36bc828dc2eedcfa73a
Julian Pollinger <julian@pollinger.dev>
2025-12-23 17:06:45 +0100

    feat: create behaviour for success page

---
+++ src/edi/formactions/behaviors/success_page.py
@@ -0,0 +1,43 @@
+from edi.jsonforms.content.form import IForm
+from zope.interface import provider
+from edi.formactions import _
+from plone.supermodel import model
+from plone.supermodel.directives import fieldset
+
+from plone.app.textfield import RichText
+from zope.interface.declarations import implementer
+from zope.interface import Interface
+
+from plone.base.utils import safe_hasattr
+
+class ISuccessPageMarker(Interface):
+    pass
+
+class ISuccessPage(model.Schema):
+
+    fieldset(
+        "success_page",
+        label=_("Success Page"),
+        fields=[
+            "success_page"
+        ],
+    )
+    success_page = RichText(
+        title=_("Success Page"),
+        required=False
+    )
+
+@implementer(ISuccessPage)
+class SuccessPage(object):
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def success_page(self):
+        if safe_hasattr(self.context, "success_page"):
+            return self.context.success_page
+        return None
+
+    @success_page.setter
+    def success_page(self, value):
+        self.context.success_page = value
