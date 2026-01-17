34fad88f4791324e28a2c845b6907853a3c3c96e
Lars Walther <lwalther@novareto.de>
2026-01-17 16:53:41 +0100

    Back to development: 0.10

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='edi.formactions',
-    version='0.9',
+    version='0.10.dev0',
     description="Action package for edi.jsonforms",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
