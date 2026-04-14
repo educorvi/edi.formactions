36952899cda8918de5505fd436ca8fdb9ab5da0a
Nina Mücke <nina.muecke@educorvi.de>
2026-04-14 12:50:42 +0200

    feat: add potenzialanalyse-view for folder with jsonformsdocuments

--- src/edi/formactions/views/potenzialanalyse_view.py
+++ src/edi/formactions/views/potenzialanalyse_view.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*-
-
 # from edi.formactions import _
+import json
+
 from edi.formactions.views.annotations_view import AnnotationsView
 from zope.interface import implementer
 from zope.interface import Interface
+from Products.Five.browser import BrowserView
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
@@ -21,8 +22,42 @@ mapping = {
 }
 
 
+def create_service(json_data: dict) -> dict:
+    return {
+        "service_id": json_data.get("service_id", ""),
+        "service_title": json_data.get("service_title", "")
+        or json_data.get("service_title_new", ""),
+        "service_description": json_data.get("service_description_new", ""),
+        "mandant": json_data.get("mandant", ""),
+        "bearbeitung_durch": json_data.get("bearbeitung_durch", ""),
+        "fachbereich": json_data.get("fachbereich", ""),
+        "status": json_data.get("status", ""),
+        "fachdomaene": json_data.get("fachdomaene", ""),
+        "fachbereich_subdomaene": json_data.get("mub_fachbereich", "")
+        or json_data.get("subdomaenen_rul", "")
+        or json_data.get("subdomaenen_praev", ""),
+        "querschnittsthemen": json_data.get("querschnittsthemen", ""),
+        "list_potenziale": json_data.get("list_potenziale", ""),
+        "potenziale_weitere": json_data.get("potenziale_weitere", ""),
+        "browser": mapping.get(json_data.get("browser", ""), ""),
+        "app_pwa": mapping.get(json_data.get("app_pwa", ""), ""),
+        "api": mapping.get(json_data.get("api", ""), ""),
+        "chatbot": json_data.get("chatbot", ""),
+        "ki_assistenz": json_data.get("ki_assistenz", ""),
+        "automatisierung": json_data.get("automatisierung", ""),
+        "aufwand": json_data.get("aufwand", ""),
+        "ideen": json_data.get("ideen", ""),
+    }
+    # servicelandkarte_ja_nein ?
+    # funktion ?
+
+
 @implementer(IPotenzialanalyseView)
-class PotenzialanalyseView(AnnotationsView):
+class AnnotationsPotenzialanalyseView(AnnotationsView):
+    """
+    View for a form (IForm) that contains annotations with the form data of the filled form (created by the formaction annotation_storage_handler)
+    """
+
     def __call__(self):
         return self.index()
 
@@ -30,38 +65,29 @@ class PotenzialanalyseView(AnnotationsView):
         annotations = self.get_annotations()
         services = []
         for annotation in annotations:
-            annotation = annotation["json_data"]
-            import pdb
+            json_data = annotation["json_data"]
+            service = create_service(json_data)
+            services.append(service)
+        return services
 
-            pdb.set_trace()
-            service = {
-                "service_id": annotation.get("service_id", ""),
-                "service_title": annotation.get("service_title", "")
-                or annotation.get("service_title_new", ""),
-                "service_description": annotation.get("service_description_new", ""),
-                "mandant": annotation.get("mandant", ""),
-                "bearbeitung_durch": annotation.get("bearbeitung_durch", ""),
-                "fachbereich": annotation.get("fachbereich", ""),
-                "status": annotation.get("status", ""),
-                "fachdomaene": annotation.get("fachdomaene", ""),
-                "fachbereich_subdomaene": annotation.get("mub_fachbereich", "")
-                or annotation.get("subdomaenen_rul", "")
-                or annotation.get("subdomaenen_praev", ""),
-                "querschnittsthemen": annotation.get("querschnittsthemen", ""),
-                "list_potenziale": annotation.get("list_potenziale", ""),
-                "potenziale_weitere": annotation.get("potenziale_weitere", ""),
-                "browser": mapping.get(
-                    annotation.get("browser", ""), ""
-                ),  # map to bedeutung_kommunikationskanaele
-                "app_pwa": mapping.get(annotation.get("app_pwa", ""), ""),
-                "api": mapping.get(annotation.get("api", ""), ""),
-                "chatbot": annotation.get("chatbot", ""),
-                "ki_assistenz": annotation.get("ki_assistenz", ""),
-                "automatisierung": annotation.get("automatisierung", ""),
-                "aufwand": annotation.get("aufwand", ""),
-                "ideen": annotation.get("ideen", ""),
-            }
-            # servicelandkarte_ja_nein ?
-            # funktion ?
+
+@implementer(IPotenzialanalyseView)
+class JFDPotenzialanalyseView(BrowserView):
+    """
+    View for a folder that contains JsonFormsDocuments (created by the formaction file_storage_handler)
+    """
+
+    def __call__(self):
+        return self.index()
+
+    def get_services(self):
+        jfds = self.context.restrictedTraverse("contentlisting")()
+        services = []
+        for jfd in jfds:
+            if jfd.portal_type != "JsonFormsDocument":
+                continue
+            json_data_str = getattr(jfd, "json_data", "{}")
+            json_data = json.loads(json_data_str)
+            service = create_service(json_data)
             services.append(service)
         return services
